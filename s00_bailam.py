ba2aef79e13c204c2376567c9a9b8523425c32c5
namgivu <namgivu@gmail.com>
2023-10-08 22:16:45 +0000

    update template

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,17 +1,18 @@
 """
---- debai 
-Viet ham 
-def hi(name):
-  # tra ve cau chao 'Hi {name}!'
+--- debai / problem
+abb ccc
 
---- vidu
+--- vidu mau / testcase
 Khi chay voi input | Ketqua output
 ------------------ | ------------
-hi('AI-BTX')       | Hi AI-BTX!
-hi('HSU')          | Hi HSU!
-hi(None)           | Hi!
-hi('')             | Hi!
+abb ccc            | xyy zzz
+abb ccc            | xyy zzz
 """
 
-def hi(name):
-  return 'TODO'
+def sumoftwo(i1,i2):
+  return i1+i2
+
+
+#CAUTION don't delete these 3 lines
+# please help set this solution to your method above - hãy gán solution bằng tên hàm của bạn ở trên
+solution = sumoftwo
