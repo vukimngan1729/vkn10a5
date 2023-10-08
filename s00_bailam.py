7e051dcfa43968c63f6418063cef12594e8b1770
namgivu <namgivu@gmail.com>
2023-10-08 17:00:09 +0000

    bailam w/ debai having 4 tc

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,11 +1,18 @@
 """
 --- debai 
-Viet python ham hi()
-Tra ve cau chao 'Hi'
+Viet ham 
+def hi(name):
+  # tra ve cau chao 'Hi {name}!'
 
---- input 
-(ko co)
-
---- output 
-Hi!
+--- vidu
+Khi chay voi input | Ketqua output
+------------------ | ------------
+hi('AI-BTX')       | Hi AI-BTX!
+hi('HSU')          | Hi HSU!
+hi(None)           | Hi!
+hi('')             | Hi!
 """
+
+def hi(name):
+  return 'TODO'
+  
