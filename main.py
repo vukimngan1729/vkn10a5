2ed0b1a2b4fedf4f10157ea0f094c9a6a6c1578b
namgivu <namgivu@gmail.com>
2023-10-08 15:48:45 +0000

    testkey_list

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from s02_chambai import chambai
 
-print(f'''
-Ketqua chambai
-{ chambai() }
-'''.strip())
+
+testkey_list = [
+    {'tc_name': 'tc0', 'input': 'AI BTX', 'output':'Hi AI BTX'},  
+    {'tc_name': 'tc1', 'input': None,     'output':'Hi'},    
+    {'tc_name': 'tc2', 'input': '',       'output':'Hi'},
+]
+
+for tc in testkey_list:  # tc aka testcase
+  INP_name = tc['input']
+  tc_score = chambai(INP_name)
+  print(f'''{tc['tc_name']} {tc_score}''')
