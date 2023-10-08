35b8795f89e86e5d23f4fd893b8e5ab4fab817e2
namgivu <namgivu@gmail.com>
2023-10-08 16:32:34 +0000

    TODO take into account o_TESTCASE=testcase['output'] ie what if o_TESTCASE != o_DAPAN, often we must have o_TESTCASE == o_DAPAN

--- s02_chambai.py
+++ s02_chambai.py
@@ -6,5 +6,7 @@ def chambai(testcase, dapan_f, bailam_f):
   except:
     o_BAILAM = None
 
+  #TODO take into account o_TESTCASE=testcase['output'] ie what if o_TESTCASE != o_DAPAN, often we must have o_TESTCASE == o_DAPAN 
+  
   score01 = int(o_BAILAM == o_DAPAN)
   return score01, o_DAPAN, o_BAILAM
