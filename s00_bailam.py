6dd58439487116a98832f64c6827cc7171a5b00b
Nam G VU <namgivu@gmail.com>
2023-10-09 17:35:29 +0700

    result from generated code

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,20 +1,21 @@
-#region debai
 """
+--- ma debai / id
+sumtwoint(i1, i2)
+
 --- debai / problem
-abb ccc
+Hay viet ham sumtwoint(i1, i2) tinh tong hai so i1, i2
 
 --- vidu mau / testcase
-Khi chay voi input | Ketqua output
------------------- | ------------
-abb ccc            | xyy zzz
-abb ccc            | xyy zzz
+Khi chay voi input    | Ketqua output
+--------------------- | ------------
+sumtwoint(1, 22)      | 23
+sumtwoint(1, None)    | None
+sumtwoint(None, 22)   | None
+sumtwoint(None, None) | None
 """
-#endregion debai
 
-#region bailam
-def sumtwoint(i1,i2):
-  return i1+i2
-#endregion bailam
+def sumtwoint(i1, i2):
+  return 'todo'
 
 #CHU Y khong xoa cac dong nay
 #          sumtwoint <-- hay thay bang ten ham cua ban o tren
