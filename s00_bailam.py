a1fd7874a102b2e88f9624e8dbb700b7cecdd256
Nam G VU <namgivu@gmail.com>
2023-10-09 16:51:23 +0700

    bailam template

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,3 +1,4 @@
+#region debai
 """
 --- debai / problem
 abb ccc
@@ -8,11 +9,13 @@ Khi chay voi input | Ketqua output
 abb ccc            | xyy zzz
 abb ccc            | xyy zzz
 """
+#endregion debai
 
-def sumoftwo(i1,i2):
+#region bailam
+def sumtwoint(i1,i2):
   return i1+i2
+#endregion bailam
 
-
-#CAUTION don't delete these 3 lines
-# please help set this solution to your method above - hãy gán solution bằng tên hàm của bạn ở trên
-solution = sumoftwo
+#CHU Y khong xoa cac dong nay
+#          sumtwoint <-- hay thay bang ten ham cua ban o tren
+solution = sumtwoint
