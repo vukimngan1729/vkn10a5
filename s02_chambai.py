fd6768097af92ba98aedde8535950fc60c979ad0
namgivu <namgivu@gmail.com>
2023-10-09 10:38:03 +0000

    fix :output check in tc

--- s02_chambai.py
+++ s02_chambai.py
@@ -1,6 +1,6 @@
 def chambai(testcase, bailam_f):
+  assert 'output' in testcase, 'MUST HAVE :output in testcase to score :bailam ^^'
   o_TESTCASE = testcase.get('output')
-  assert o_TESTCASE, 'MUST HAVE :output in testcase to score :bailam ^^'
 
   assert callable(bailam_f), 'MUST HAVE bailam_f() method to score :bailam ^^'
   
