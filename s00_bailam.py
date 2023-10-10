437f3e84667e24c40dccd424a62594d45ff3d526
Nam G VU <namgivu@gmail.com>
2023-10-10 12:49:33 +0700

    nopbai @ generated bailam

--- s00_bailam.py
+++ s00_bailam.py
@@ -3,6 +3,16 @@
 --- ma debai / id
 sumtwoint(i1, i2)
 
+--- nopbai
+00 fork tu bainopmau tren replit tu trang web duoiday 
+   https://replit.com/@NamG1/bainopmau2310090444sumtwoint
+
+01 lam bai vao tep s00_bailam.py, chay Run de co ketqua tatca la 1
+02a tao github repo, mo kiemtra tep s00_bailam.py, va lay diachi/url aka githubrepourl
+
+02b dan diachi githubrepourl tu trang web duoiday
+    https://forms.gle/zWyfJ5wGdqsstLcA6
+
 --- debai / problem
 Hay viet ham sumtwoint(i1, i2) tinh tong hai so i1, i2
 
